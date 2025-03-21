58ccefc2fe292f07649427750d208bf525c5a14a
shubham sharma <shubhamtech004@gmail.com>
2025-03-21 16:49:22 +0530

    styling using the saas

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Styles from "../styles/about.module.scss"
 function About() {
   return (
-    <div>Response from About</div>
+    <div>
+    <p className={Styles.para}>Response From Contact page </p>
+    <p className={Styles.heading}>ok Response</p>       
+</div>
   )
 }
 
